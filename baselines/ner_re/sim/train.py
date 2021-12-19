d6dd4bc246853447bc6ab575a5ceb7de1807bf0f
monster-star <1194370384@qq.com>
2021-12-19 14:35:08 +0800

    修改部分小问题，并验证代码可用性

--- baselines/ner_re/sim/train.py
+++ baselines/ner_re/sim/train.py
@@ -1,11 +1,14 @@
 #! -*- coding:utf-8 -*-
 # 句子对分类任务，KgCLUE数据集
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '4'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+else:
+    print("Not enough GPU hardware devices available")
+    print("Use CPU to train sim")
 import numpy as np
 from bert4keras.backend import keras, set_gelu, K
 from bert4keras.tokenizers import Tokenizer
